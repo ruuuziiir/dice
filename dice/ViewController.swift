4a8ed8cc7948b8c43360943ba74c9f079541aa1e
7 <ruuuziiir@gmail.com>
2021-01-04 19:28:55 +0800

    Initial Commit

---
+++ dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dice
+//
+//  Created by 7 on 2021/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
